010f551c3be6d0a9a637a4aff803e6f62b114746
luke-h1 <luke.howsam@yahoo.com>
2020-09-03 21:24:52 +0100

    update to proj

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ async function getData(e) {
     showError('Please enter a correct character');
   } else {
     showLoading();
-    const REQ_URL = `${API_URL}${inputQueryValue}`;
+    const REQ_URL = `${API_URL}${inputQueryValue}`; // base url + input value
     const res = await fetch(`${REQ_URL}`);
     const data = await res.json();
     console.log(data);
     hideLoading();
+    showData(data);
   }
 }
 
@@ -40,7 +41,35 @@ function showError(message) {
 }
 
 // show data in DOM
-function showData() {}
+function showData(data) {
+  let output = '';
+  data.results.forEach((character) => {
+    output += `
+    <div class="wrapper">
+      <img src="" class="wrapper-img-thumbnail" alt="a star wars character" />
+      <h4 class="wrapper-title">star wars character here</h4>
+      <ul class="wrapper-films">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li>Film character appears in</li>
+      </ul>
+      <br />
+      <hr />
+      <ul class="wrapper-vehicles">
+        <li>name of Vehicle character uses</li>
+        <li>specific model name</li>
+        <li>Price of vehicle</li>
+        <li>crew</li>
+      </ul>
+    </div>
+    
+    
+    
+    
+    `;
+  });
+}
 
 // EVENT LISTENERS
 form.addEventListener('submit', getData);
