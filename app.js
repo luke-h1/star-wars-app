e501508072786245b2c24b8bf1f3a5a54fc5bb33
luke-h1 <luke.howsam@yahoo.com>
2020-09-03 21:20:26 +0100

    add err handler & getting data back from api

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-const form = document.getElementById('form-control'); 
+const form = document.getElementById('form-control');
 const userInput = document.getElementById('form-control--input');
 const outputDiv = document.getElementById('output-flex');
 const errorDiv = document.getElementById('error');
-const API_URL = `https://swapi.dev/api/people/?search=${character}`;
+const loadingGif = document.getElementById('loading');
+const API_URL = `https://swapi.dev/api/people/?search=`;
 
+async function getData(e) {
+  e.preventDefault();
+  const inputQueryValue = userInput.value;
+  console.log(inputQueryValue);
+  if (inputQueryValue === '') {
+    showError('Please enter a correct character');
+  } else {
+    showLoading();
+    const REQ_URL = `${API_URL}${inputQueryValue}`;
+    const res = await fetch(`${REQ_URL}`);
+    const data = await res.json();
+    console.log(data);
+    hideLoading();
+  }
+}
 
+// show loading gif
+function showLoading() {
+  loadingGif.classList.add('show');
+}
 
+// hide loading gif
+function hideLoading() {
+  loadingGif.classList.remove('show');
+}
 
+// show error msg
+function showError(message) {
+  errorDiv.innerHTML = message;
+  setTimeout(() => {
+    errorDiv.innerHTML = '';
+  }, 2000);
+}
 
+// show data in DOM
+function showData() {}
 
-
-
-// EVENT LISTENERS 
+// EVENT LISTENERS
 form.addEventListener('submit', getData);
